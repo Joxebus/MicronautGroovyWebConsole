cecf0f6843e902672700141f91092c68568c0cbc
Omar Bautista <joxebus@gmail.com>
2021-02-18 20:21:46 -0600

    Add shortcuts for execute, clear output and clear all

--- src/main/resources/public/js/home.js
+++ src/main/resources/public/js/home.js
@@ -9,21 +9,14 @@ function init() {
         mode: "text/x-groovy"
     });
 
-    document.getElementById("clear").onclick = function() {
-        console.log("Clearing code and output")
-        editor.setValue("");
-        editor.clearHistory();
-        resetOutputSection();
-    }
+    document.getElementById("clear-output").onclick = resetOutputSection;
+    document.getElementById("clear-all").onclick = clearAll;
+    document.getElementById("execute").onclick = executeCode;
 
-    document.getElementById("execute").onclick = function() {
+    function executeCode() {
         console.log("Executing script");
         resetOutputSection();
-        executeCode(editor.getValue());
-    }
-
-    function executeCode(code) {
-        const requestData = { code: code  }
+        const requestData = { code: editor.getValue()  }
         const xhr = new XMLHttpRequest();
         xhr.open("POST", "/", true);
         xhr.setRequestHeader('Content-Type', 'application/json');
@@ -45,10 +38,36 @@ function init() {
 
     }
 
+    function clearAll() {
+        console.log("Clearing code and output")
+        editor.setValue("");
+        editor.clearHistory();
+        resetOutputSection();
+    }
+
     function resetOutputSection() {
+        document.getElementById("executionTime").innerText = "";
         document.getElementById("output").innerText = "";
         document.getElementById("error").innerText = "";
         document.getElementById("output-card").hidden = false;
         document.getElementById("error-card").hidden = true;
     }
+
+    function keyPressShortcuts(e) {
+        if(e.ctrlKey) {
+            switch (e.keyCode) {
+                case 82: // R
+                    executeCode();
+                    break;
+                case 87: // W
+                    resetOutputSection();
+                    break;
+                case 75: // K
+                    clearAll();
+                    break;
+            }
+        }
+    }
+
+    document.onkeydown = keyPressShortcuts;
 }
